d1886af1b6288548e39b2a5b2e9a6638d5deff1d
Jörg Weisbarth <joerg.weisbarth@sap.com>
2023-02-15 15:20:36 +0100

    Improve descriptions;

--- decSysConf/flake.nix
+++ decSysConf/flake.nix
@@ -1,17 +1,7 @@
-# Filename: ~/.config/nixpkgs/flake.nix
-#
-# This is a declarative Nix-Configuration in the spirit of
-# https://nixos.org/manual/nixpkgs/stable/#sec-declarative-package-management
-# and adapted to nix-flakes.
+# This is a declarative Nix-Configuration created from the template `github:joergdw/nix-templates#decSysConf`.
 #
 # For this to work with the new nix-cli, you need to activate flakes, see:
 # https://nixos.wiki/wiki/Flakes#Enable_flakes
-#
-# Subsequently, it is assumed that you use the new nix-cli. For using the old one
-# instead, please load this flake into your package-overrides, see:
-#  + https://nixos.org/manual/nixpkgs/stable/#sec-declarative-package-management
-#  + https://discourse.nixos.org/t/nix-profile-in-combination-with-declarative-package-management/21228/6
-#  + https://discourse.nixos.org/t/nix-profile-in-combination-with-declarative-package-management/21228/7
 #
 # Installed packages can be listet with `nix profile list`.
 # To install a certain package defined in the flake, change to the directory the flake resides in and use:
@@ -19,7 +9,7 @@
 # To update all packages, use from within the same folder `nix flake update && nix profile upgrade '.*'`.
 # Don't forget to perform sometimes a garbage-collection when your system is in a fine state.
 {
-  description = "A declarative system installation using nix-flakes";
+  description = "A declarative system installation using nix-flakes;";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11"; # also possible: `"github:NixOS/nixpkgs/nixos-unstable";`
@@ -27,7 +17,10 @@
 
   outputs = { self, nixpkgs }:
     let
-      # Choose another system, in case this is not yours, e.g.: "x86_64-darwin" "aarch64-linux" "aarch64-darwin"
+      # Choose another system, in case this is not yours.
+      # You should be able to get it via executing `nix eval --impure --raw --expr 'builtins.currentSystem'`
+      # on a terminal.
+      # Other common values are "x86_64-darwin", "aarch64-linux" and "aarch64-darwin".
       # For a complete list of valid systems, see:
       # <https://github.com/numtide/flake-utils/blob/04c1b180862888302ddfb2e3ad9eaa63afc60cf8/default.nix>
       supported_system = "x86_64-linux";
@@ -35,7 +28,10 @@
       packages = {
         ${supported_system} =
           let
-            pkgs = import nixpkgs { system = supported_system; };
+            pkgs = import nixpkgs {
+              system = supported_system;
+              # config.allowUnfree = true; # Set this, if you want to include non-free software.
+            };
           in {
             default = self.packages.${supported_system}.myStdPackages;
 
@@ -57,7 +53,7 @@
                 # somewhere in the `"${HOME}/.profile"` or `"${HOME}/.bash_profile"` or `"${HOME}/.zshenv"`.
                 #
                 # You can verify the inclusion of the revelant directories via `manpath --debug`.
-      
+                #
                 # Regarding shell-completion:
                 # With bash, everything should work out of the box.
                 # With zsh, the following needs to be done somewhere in the `"${HOME}/.zshenv"`:
@@ -69,5 +65,7 @@
             #myRustPackages = …
           };
       }; # packages
+
+      # TODO: Integrate: https://www.tweag.io/blog/2020-07-31-nixos-flakes
     }; # outputs
 }
